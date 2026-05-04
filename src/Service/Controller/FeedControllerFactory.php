615df4b32cea91c51e14bad44c63c5115abb0bb8
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-05-04 00:00:00 +0000

    Replaced Interop\Container by standard Psr\Container.

--- src/Service/Controller/FeedControllerFactory.php
+++ src/Service/Controller/FeedControllerFactory.php
@@ -3,7 +3,7 @@
 namespace Feed\Service\Controller;
 
 use Feed\Controller\FeedController;
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 
 class FeedControllerFactory implements FactoryInterface
